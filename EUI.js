83377142a77db85d4be63767f908bd0a0181424a
zhangjie <3081457329@qq.com>
2022-05-07 16:34:29 +0800

    EUI1.0.0版本完成

--- EUI.js
+++ EUI.js
@@ -5,7 +5,7 @@ const $ = {
     pwd: /^[a-zA-Z0-9_@-]{6,16}$/,
     tel: /^[0-9]{11}$/
 };
-//封装log
+
 function log(logStr) {
     return console.log(logStr);
 }
@@ -17,23 +17,3 @@ function dir(dirStr) {
 function write(wStr) {
     return window.document.write(wStr);
 };
-
-function html(into, out) {
-    into.innerHTML = out;
-}
-//添加子元素
-function bind(obj, event, callback) {
-    obj.bind()
-    obj.addEventListener(event, callback);
-    return this;
-}
-/*
-//事件构造函数
-function Element() {};
-Element.prototype.add = (ele, location, node) => {
-    //创建元素并添加
-    let para = document.createElement(ele);
-    let node = document.createTextNode("这是新文本。");
-    location.appendChild(node);
-    return this;
-} */
